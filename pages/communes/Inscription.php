f32dec7f92dd9a68e8b54f94614035efb7f6ed26
brownChristopher <browniechoco36@gmail.com>
2017-08-30 13:40:02 -0400

    inscription
    début

---
+++ pages/communes/Inscription.php
@@ -0,0 +1,35 @@
+<!-- /**************************************************************************************************/
+/* Fichier ...................... : Inscription.php */
+/* Type ......................... : Document PHP */
+/* Titre ........................ : Inscription*/
+/* Auteur ....................... : Christopher Brown */
+/* Date de création ............. : 2017-08-30 */
+/* Date de mise en ligne ........ : 2017-08-30 */
+/* Date de mise à jour .......... : 2017-08-30 */
+/*******************************************************************************************************/
+/* Inscription pour un utilisateur */
+/*******************************************************************************************************/
+-->
+<!doctype html>
+<html lang="fr">
+	
+	<head>
+		<meta charset="utf-8">
+		<title>Connexion</title>
+		<link rel="stylesheet" href="../../styles/style.css"/>
+	</head>
+	
+	<header>
+		<?php include_once '../Entete.php' ?>
+	</header>
+	
+	<body>
+		
+		
+		
+	</body>
+	
+	<footer>
+	</footer>
+	
+</html>
