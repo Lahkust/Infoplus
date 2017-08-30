0bd5c02cca0c0f7c9114d4b0444699fad83b7a18
Guillaume Bergs <guillaume.robert.bergs@gmail.com>
2017-08-30 12:43:13 -0400

    Promos et images
    objectification des promotions
    renommage des images de promotion

---
+++ objects/Promotion.php
@@ -0,0 +1,49 @@
+ <?php
+        
+        class Promotion {
+			
+            public $date_debut;
+			public $date_fin;
+			public $code;
+			public $titre;
+			public $rabais;
+			public $service_titre;
+            
+            public function __construct($row) {
+              $this->date_debut  	= $row["date_debut"];
+              $this->date_fin 		= $row["date_fin"];
+              $this->code 		 	= $row["code"];
+              $this->titre 		 	= $row["promotion_titre"];
+              $this->rabais 	 	= $row["rabais"];
+            }
+			
+            public function isPast() {
+              return (time() - strtotime($this->date_fin)) > 0;
+            }
+			
+			public function isFuture() {
+              return (time() - strtotime($this->date_debut)) < 0;
+            }
+			
+			public function isCurrent() {
+              return !($this->isPast() || $this->isFuture());
+            }
+			
+			public function insertImg() {
+				$s = "<img src='../../images/promotions/".$this->rabais.".png' class=";
+				
+			  if($this->isPast())
+			  {
+				print_r($s."'promo_past'/>");
+			  }
+			  else if($this->isPresent())
+			  {
+				print_r($s."'promo_current'/>");
+			  }
+			  else
+			  {
+				print_r($s."'promo_future'/>");
+			  }
+			}
+          }
+        ?>
--- pages/administrateurs/Service.php
+++ pages/administrateurs/Service.php
@@ -28,28 +28,30 @@
 	<body>
 		
 <?php
+include_once '../../objects/Promotion.php';
 	try {
 		$dbh = new PDO('mysql:host=localhost;dbname=infoplus', 'root', '');
 		foreach($dbh->query('SELECT * FROM service') as $row) {
-			
 			print_r("<div class='service_entry'>");
-				print_r("<img src='../../images/services/" . $row["image"]. ".png' class='img_service'/><br/>");
-				print_r("<div class='service_title'>" . $row["service_titre"]. "</div><br/>");
-				print_r("<div class='service_description'>" . $row["service_description"]. "</div><br/>");
-				print_r("<div class='service_price'>" . $row["tarif"]. "</div><br/>");
-				print_r("<div class='service_duration'>" . $row["duree"]. "</div><br/>");
-				print_r("<div class='promotion_title'>Promotions</div><br/>");
+			print_r("<img src='../../images/services/" . $row["image"]. ".png' class='img_service'/><br/>");
+			print_r("<div class='service_title'>" . $row["service_titre"]. "</div><br/>");
+			print_r("<div class='service_description'>" . $row["service_description"]. "</div><br/>");
+			print_r("<div class='service_price'>" . $row["tarif"]. "$</div><br/>");
+			print_r("<div class='service_duration'>" . $row["duree"]. "h</div><br/>");
+			print_r("<div class='promotion_title'>Promotions</div><br/>");
+			
+			$promos;
+			foreach($dbh->query('SELECT * FROM promotion JOIN ta_promotion_service ON promotion.pk_promotion = ta_promotion_service.fk_promotion AND ta_promotion_service.fk_service = '.$row["pk_service"]) as $row2) {
 				
-				
-				foreach($dbh->query('SELECT * FROM promotion JOIN ta_promotion_service ON promotion.pk_promotion = ta_promotion_service.fk_promotion AND ta_promotion_service.fk_service = '.$row["pk_service"]) as $row2) {
+				$promos[$row2["pk_promotion_service"]] = new Promotion($row2);
 					
+				$promos[$row2["pk_promotion_service"]]->insertImg();
+			}
+			
+			print_r("<div class='btn_add'><a href='http://www.perdu.com'><img src='../../images/icones/plus.png' class='btn_add'/></a></div>");
+			
+			print_r("<div class='btn_sociaux'><a href='http://www.perdu.com'><img src='../../images/icones/medias sociaux.jpeg' class='btn_sociaux'/></a></div>");
 					
-					print_r($row2["rabais"]);
-				}
-				
-				print_r("<div class='btn_add'><a href='http://www.perdu.com'><img src='../../images/icsones/medias sociaux.jpeg' class='btn_add'/></a></div>");
-				
-				
 			print_r("</div");
 			print_r("<br/><br/>");
 		}
