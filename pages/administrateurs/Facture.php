42e69e0799ed4f67f29d94aa69b666ebed7333f8
brownChristopher <browniechoco36@gmail.com>
2017-09-30 14:30:18 -0400

    maj facture

--- pages/administrateurs/Facture.php
+++ pages/administrateurs/Facture.php
@@ -37,14 +37,12 @@
 		<!--Verifie s'il y a un utilisateur de connecte-->
 		<?php try {
 			$i = 0;
-			$dbh = db_connect();		
-			/*$dbh = new PDO('mysql:host=localhost;dbname=infoplus', 'root', '');
-			$dbh->exec("set names utf8");*/
-			foreach($dbh->query('select * from client a join facture b on a.pk_client=b.fk_client join ta_facture_service c 
+			$dbh1 = db_connect();		
+			foreach($dbh1->query('select * from client a join facture b on a.pk_client=b.fk_client join ta_facture_service c 
 				on b.pk_facture=c.pk_facture_service ORDER BY b.date_service DESC') as $row) {
 				$i++;
 				?>
-				<div class="container-fluid">
+				<div class="container-fluid service_entry">
 					<div class="row">
 						<div class="col-md-12">
 							<div class="row">
@@ -71,45 +69,94 @@
 											<?php print_r($row["no_confirmation"]); ?>
 										</div>
 										<div class="col-md-3">
-											
-											<?php print_r($row["tarif_facture"]); ?>
+										<?php
+										try {
+											$pk_facture = $row["pk_facture"];
+											$stmt = $dbh1->prepare('SELECT SUM(tarif_facture) FROM ta_facture_service WHERE fk_facture = :fk_facture');
+											$stmt->execute(['fk_facture' => $pk_facture]);
+											$facture = $stmt->fetch();
+											print $facture['SUM(tarif_facture)'];
+										} catch (PDOException $e) {
+											print "Error!: " . $e->getMessage() . "<br/>";
+											die();
+										}
+										?>
 										</div>
 										<div class="col-md-2">
 											 <a id="hide" class="collapsed" data-toggle="collapse" <?php echo 'href="#detail' . $i . '"';?> ><!--onclick="hideDetail()"-->Détail</a>	
 										</div>
 									</div>
-									<!-- requete sql pour avoir le nombre de rows -->
-									
-										<div <?php echo 'id="detail' . $i . '"';?> class="collapse">
+									<div <?php echo 'id="detail' . $i . '"';?> class="collapse">
+									<?php
+									//Avoir les détails sur les produits achetés
+									try {
+										$stmt = $dbh1->prepare('SELECT * FROM ta_facture_service a join service b on a.fk_service = b.pk_service WHERE fk_facture = :fk_facture');
+										$stmt->execute(['fk_facture' => $pk_facture]);
+										$data = $stmt->fetchAll();
+										foreach ($data as $row1)
+										{
+										?>
 											<div class="row">
 												<div class="col-md-3">
-													</div>
-													<div class="col-md-4">
-														oo
-													</div>
-													<div class="col-md-3">
-														kk
-													</div>
-													<div class="col-md-2">
-														 <a class="collapsed" data-toggle="collapse"  href="#detail" <!--onclick="hideDetail()"-->>Réduire</a>
-													</div>
+												</div>
+												<div class="col-md-4">
+													<?php echo $row1['service_titre']; ?>
+												</div>
+												<div class="col-md-3">
+													<?php echo $row1['tarif']; ?>
+												</div>
+												<div class="col-md-2">
+													 <a class="collapsed" data-toggle="collapse"  href="#detail" <!--onclick="hideDetail()"Réduire--></a>
 												</div>
 											</div>
-										</div>
+											<?php
+											$service = $row1['pk_service'];
+											$stmt = $dbh1->prepare('SELECT pk_service, promotion_titre, rabais FROM service a join ta_promotion_service b 
+											on a.pk_service = b.fk_service join promotion c on b.fk_promotion=c.pk_promotion');
+											$stmt->execute(['fk_facture' => $service]);
+											$data1 = $stmt->fetchAll();
+											$facture = $stmt->fetch();
+											print $facture['SUM(tarif_facture)'];
+											?>
+												<div class="row">
+													<div class="col-md-3">
+													</div>
+													<div class="col-md-4">
+														<?php echo $row1['service_titre']; ?>
+													</div>
+													<div class="col-md-3">
+														<?php echo $row1['tarif']; ?>
+													</div>
+													<div class="col-md-2">
+														 <a class="collapsed" data-toggle="collapse"  href="#detail" <!--onclick="hideDetail()"Réduire--></a>
+													</div>
+												</div>
+											<?php
+											}
+										<?php
+										}
+									} catch (PDOException $e) {
+										print "Error!: " . $e->getMessage() . "<br/>";
+										die();
+									}
 									
+									?>
+									</div>
 								</div>
-								<div class="col-md-1">
-								</div>
+							</div>
+							<div class="col-md-1">
 							</div>
 						</div>
 					</div>
-				</div><?php
+				</div>
+				<?php
 			}
-			$dbh = null;
+			
 		} catch (PDOException $e) {
 			print "Error!: " . $e->getMessage() . "<br/>";
 			die();
 		}
+		$dbh1 = null;
 		?>
 		
 		<script>
