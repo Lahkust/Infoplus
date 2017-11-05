45ef0c21e9366c8f1b61ac199eebebba1a4b684e
Guillaume Bergs <guillaume.robert.bergs@gmail.com>
2017-11-05 14:55:23 -0500

    page confirmation

--- pages/clients/Confirmation.php
+++ pages/clients/Confirmation.php
@@ -12,7 +12,11 @@
 -->
 <!doctype HTML>
 <html lang="fr">
-<?php	 session_start(); ?>
+<?php	 session_start();
+require_once '../../Objects/Connection.php';
+$dbh = db_connect();
+
+?>
 
 <head>
     <meta charset="utf-8">
@@ -31,17 +35,136 @@
 </header>
 
 <div class="container-fluid">
+
     <div class="row">
+        <div class="col-md-4"></div>
+        <div class="col-md-4 merci">Merci pour votre achat!</div>
+        <div class="col-md-4"></div>
+    </div>
+
+    <div class="row">
+        <div class="col-md-4"></div>
+        <div class="col-md-4 merci">Voici le résumé de votre commande:</div>
+        <div class="col-md-4"></div>
+    </div>
+
+    <div class="row">
+        <hr/>
+    </div>
+
+    <?php
+    $sousTotal = 0;
+    $rabaisTotal = 0;
+    $promoAdditionnelle = 0;
+    $id_service_panier_tableau = 0;
+    try {
+    foreach($_SESSION["panier"] as $id_service_panier)
+    foreach($dbh->query('SELECT * from service WHERE pk_service = '.$id_service_panier) as $row) {
+    ?>
+    <div class='row'>
+
+        <div class='col-1'></div>
+
+        <div class='col-10'>
+
+            <!-- articles -->
+            <div class='row'>
+
+                <div class='col-3'>
+
+                </div>
+
+                <div class='col-9'>
+
+                    <div class='row'>
+                        <div class='col-6'>
+                            <?php print_r("<div class='service_title'>" . $row["service_titre"]. "</div><br/>"); ?>
+                        </div>
+                        <div class='col-3'></div>
+                        <div class="col-3"><?php print_r("<div class='service_price'>" . $row["tarif"]. "$</div><br/>"); ?></div>
+                        <?php $sousTotal += $row["tarif"]?>
+                    </div>
+                    <?php
+                    $promos;
+                    //modifier le statement pour ne prendre que les promotions actives
+                    $date = date("Y-m-d");
+                    foreach($dbh->query('SELECT * FROM promotion JOIN ta_promotion_service ON promotion.pk_promotion = ta_promotion_service.fk_promotion AND ta_promotion_service.fk_service = '.$row["pk_service"]
+                        . " WHERE date_debut >= " . $date . " AND date_fin <= " . $date) as $row2) {
+
+
+
+                        //code pour afficher le prix des promotions
+                        ?>
+                        <div class="row">
+                            <div class='col-6'>
+                                <?php print_r("<div class='promo_title'>" . $row2["promotion_titre"]. "</div><br/>"); ?>
+                            </div>
+                            <div class='col-3'></div>
+                            <div class="col-3"><?php print_r("<div class='promo_price'> -". $row2["rabais"] * $row["tarif"]. "$</div><br/>"); ?></div>
+                        </div>
+                        <?php
+                        $rabaisTotal += $row2["rabais"] * $row["tarif"];
+                    }
+                    ?>
+
+                </div>
+            </div>
+
+            <?php $id_service_panier_tableau +=1;?>
+
+
+
+        </div>
+        <div class='col-1'></div>
+    </div>
+
+    <div class="row">
+        <hr/>
+    </div>
+
+
+    <?php
+    }
+    $dbh = null;
+    } catch (PDOException $e) {
+        print "Error!: " . $e->getMessage() . "<br/>";
+        die();
+    }
+?>
+
+
+    <div class="row">
+        <hr/>
+    </div>
+
+    <div class="row">
+        <div class="col-md-6"></div>
+        <div class="col-md-3">
+            Sous-Total
+        </div>
+        <div class="col-md-3">
+            <?php echo $sousTotal ?>
+        </div>
     </div>
     <div class="row">
-        <div class="col-md-4">
+        <div class="col-md-6"></div>
+        <div class="col-md-3">
+            Rabais additionnel
         </div>
-        <div class="col-md-4 err404">
-            Transaction réussie!
-        </div>
-        <div class="col-md-4">
+        <div class="col-md-3">
+            <?php echo $_SESSION["RabaisPromotionnel"] ?>
         </div>
     </div>
+    <div class="row">
+        <div class="col-md-6"></div>
+        <div class="col-md-3">
+            Total
+        </div>
+        <div class="col-md-3">
+            <?php echo $sousTotal - $_SESSION["RabaisPromotionnel"] ?>
+        </div>
+    </div>
+
 </div>
 
 <footer>
